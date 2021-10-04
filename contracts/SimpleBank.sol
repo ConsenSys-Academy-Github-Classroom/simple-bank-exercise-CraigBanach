5d868a6d88d38269bc7aea718f5e27616d187e4b
Craig Banach <craigsb93@gmail.com>
2021-10-04 20:03:38 +0100

    Final draft

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -39,7 +39,11 @@ contract SimpleBank {
 
     // Create an event called LogWithdrawal
     // Hint: it should take 3 arguments: an accountAddress, withdrawAmount and a newBalance 
-    event LogWithdrawal();
+    event LogWithdrawal(
+      address indexed accountAddress,
+      uint indexed withdrawAmount,
+      uint indexed newBalance
+    );
 
     /* Functions
      */
@@ -55,10 +59,11 @@ contract SimpleBank {
 
     /// @notice Get balance
     /// @return The balance of the user
-    function getBalance() public returns (uint) {
+    function getBalance() public view returns (uint) {
       // 1. A SPECIAL KEYWORD prevents function from editing state variables;
       //    allows function to run locally/off blockchain
       // 2. Get the balance of the sender of this transaction
+      return balances[msg.sender];
     }
 
     /// @notice Enroll a customer with the bank
@@ -110,5 +115,14 @@ contract SimpleBank {
       //    sender's balance
 
       // 3. Emit the appropriate event for this message
+
+      require(balances[msg.sender] >= withdrawAmount, "Insufficient balance for withdrawal");
+
+      msg.sender.send(withdrawAmount);
+      balances[msg.sender] -= withdrawAmount;
+
+      emit LogWithdrawal(msg.sender, withdrawAmount, balances[msg.sender]);
+
+      return balances[msg.sender];
     }
 }
